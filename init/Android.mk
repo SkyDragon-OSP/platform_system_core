omnirom/android_system_core_old

core: bring back vendor init hook
Change-Id: I46a3186b9c4f8a3e092d24f2e8e9fc3ec10ab7d8

init/Android.mk
@@ -70,6 +70,7 @@ LOCAL_SRC_FILES:= \
     signal_handler.cpp \
     ueventd.cpp \
     ueventd_parser.cpp \
+    vendor_init.c \
     watchdogd.cpp \
 
 LOCAL_MODULE:= init

SkyDragon-OSP/platform_system_core

init/Android.mk
@@ -70,6 +70,7 @@ LOCAL_SRC_FILES:= \
     signal_handler.cpp \
     ueventd.cpp \
     ueventd_parser.cpp \
+    vendor_init.cpp \
     watchdogd.cpp \
 
 LOCAL_MODULE:= init